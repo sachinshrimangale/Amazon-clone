332c102b9a62519f3e3145a8c0807b97c7492463
sachinshrimangale <160982493+sachinshrimangale@users.noreply.github.com>
2024-07-01 20:31:55 +0530

    Add files via upload

---
+++ amazon.js
@@ -0,0 +1,12 @@
+let all = document.querySelector(".panel-all");
+let searchbtn =document.querySelector(".search-input");
+searchbtn.addEventListener("click",()=>{
+    alert('Lets do your 1st shopping with us')
+})
+let footer =document.querySelector(".foot-panel4")
+footer.addEventListener("click",()=>{
+    alert('The information is encrypted for user!')
+})
+all.addEventListener("click",()=>{
+    console.log("you can see anything here!!")
+})
